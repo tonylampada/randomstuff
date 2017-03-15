ae5e5724cd152c105193d101e007a34d49d59bd9
Tony Lâmpada <tonylampada@gmail.com>
2017-03-15 11:05:31 -0300

    Create ssl.conf

---
+++ ssl.sh
@@ -0,0 +1,29 @@
+export CERTNAME=nginx   
+export CERTDAYS=3650
+
+# Adjust above to your needs
+
+
+sudo mkdir /etc/nginx/ssl
+sudo openssl req -x509 -nodes -days $CERTDAYS -newkey rsa:2048 -keyout /etc/nginx/ssl/$CERTNAME.key -out /etc/nginx/ssl/$CERTNAME.crt
+# Attention: commonname must match the domain name that will be used for the website
+
+
+# /etc/nginx/sites-enabled/yoursite.conf:
+
+# server {
+#   listen 80;
+#   server_name your_domain.com;
+#   return 301 https://$host$request_uri;
+# }
+
+# server {
+
+#     listen 443 ssl;
+#     server_name your_domain.com;
+#     ssl_certificate /etc/nginx/ssl/<CERTNAME>.crt;
+#     ssl_certificate_key /etc/nginx/ssl/<CERTNAME>.key;
+
+#     #...
+
+# }
