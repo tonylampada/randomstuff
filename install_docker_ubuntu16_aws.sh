288ffb13a7bb1a3c30850378f5ecfc9d49bb59b7
Tony Lâmpada <tonylampada@gmail.com>
2017-05-07 22:36:03 -0300

    install docker fast

---
+++ install_docker_ubuntu16_aws.sh
@@ -0,0 +1,18 @@
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+sudo docker run hello-world
+sudo usermod -aG docker ubuntu
