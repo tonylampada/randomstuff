3c36ef38ad31dc85143849e547b3b571657b9548
Tony Lâmpada <tonylampada@gmail.com>
2017-08-23 11:27:21 -0300

    docker velho

--- docker_prune.sh
+++ docker_prune.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 docker system prune -f
 docker images -q --filter "dangling=true" | xargs -r docker rmi
+## OR if old
+docker ps --filter "status=exited" | awk '{print $1}' | xargs --no-run-if-empty docker rm
+docker images -q --filter "dangling=true" | xargs -r docker rmi
