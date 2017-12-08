9740f420ca93688a903733c3227877db143bb98c
Tony Lâmpada <tonylampada@gmail.com>
2017-12-08 13:41:01 -0200

    +compose

--- install_docker_ubuntu16_aws.sh
+++ install_docker_ubuntu16_aws.sh
@@ -16,3 +16,8 @@ sudo apt-get install docker-ce
 
 sudo docker run hello-world
 sudo usermod -aG docker ubuntu
+
+COMPOSER_VERSION=1.17.1  # ver https://github.com/docker/compose/releases
+sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSER_VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
