13bcee90d4eea0f66205afa12e40b7a5412d9c52
Tony Lâmpada <tonylampada@gmail.com>
2018-03-03 10:16:24 -0300

    update compose version

--- install_docker_ubuntu16_aws.sh
+++ install_docker_ubuntu16_aws.sh
@@ -17,8 +17,7 @@ sudo apt-get install docker-ce
 sudo docker run hello-world
 sudo usermod -aG docker ubuntu
 
-COMPOSER_VERSION=1.17.1  # ver https://github.com/docker/compose/releases
-COMPOSER_VERSION=1.18.0  # ver https://github.com/docker/compose/releases
+COMPOSER_VERSION=1.19.0  # ver https://github.com/docker/compose/releases
 sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSER_VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
