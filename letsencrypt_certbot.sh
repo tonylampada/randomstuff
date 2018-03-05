fdaad2920920d45e55a017b19367f7be3daeccd4
Tony Lâmpada <tonylampada@gmail.com>
2018-03-05 00:17:45 -0300

    mete -y em tudo aih

--- letsencrypt_certbot.sh
+++ letsencrypt_certbot.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:certbot/certbot
+sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install -y python-certbot-nginx 
 sudo certbot --nginx
