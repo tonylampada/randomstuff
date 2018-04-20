57fe0a9b6b36d67e2feed5856d538392b9116c03
Tony Lâmpada <tonylampada@gmail.com>
2018-04-20 17:35:01 -0300

    Update docker_prune.sh

--- docker_prune.sh
+++ docker_prune.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker system prune -f
-docker volume prune
+docker volume prune -f
 docker images -q --filter "dangling=true" | xargs -r docker rmi
 ## OR if old
 docker ps --filter "status=exited" | awk '{print $1}' | xargs --no-run-if-empty docker rm
