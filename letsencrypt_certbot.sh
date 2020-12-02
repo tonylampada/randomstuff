cb122b81f7b7dca82653347cf6f494139396c8ad
Tony Lâmpada <tonylampada@gmail.com>
2020-12-02 15:09:06 -0300

    update letsencrypt_certbotY

--- letsencrypt_certbot.sh
+++ letsencrypt_certbot.sh
@@ -1,3 +1,5 @@
+#Ubuntu <= 18.04
+
 sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:certbot/certbot
@@ -5,3 +7,8 @@ sudo apt-get update
 sudo apt-get install -y python-certbot-nginx 
 sudo certbot --nginx
 sudo certbot renew --dry-run
+
+
+# Ubuntu >= 20.04
+sudo apt-get update
+sudo apt-get install -y software-properties-common certbot python3-certbot-nginx
