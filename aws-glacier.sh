157a98f65b594e8ec34fe146b6bcb526491a4cd0
Tony Lâmpada <218821+tonylampada@users.noreply.github.com>
2023-02-26 14:58:38 -0300

    update glacier

--- aws-glacier.sh
+++ aws-glacier.sh
@@ -4,3 +4,6 @@ aws glacier upload-archive --account-id - --vault-name tonylampada --archive-des
 
 # ou entao usando o s3
 aws s3 cp myfolder s3://mybucket/myfolder --recursive --storage-class GLACIER
+
+
+# list files https://stackoverflow.com/questions/28945085/list-files-in-glacier-with-aws-cli
