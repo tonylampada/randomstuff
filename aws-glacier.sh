49430619c4cce4f89e6e0accb2fd71a640723a5d
Tony Lâmpada <218821+tonylampada@users.noreply.github.com>
2023-02-26 13:48:02 -0300

    update glacier

--- aws-glacier.sh
+++ aws-glacier.sh
@@ -1,3 +1,6 @@
 #upload
 
 aws glacier upload-archive --account-id - --vault-name tonylampada --archive-description my-archive-description --body my-archive.tar.gz --debug
+
+# ou entao usando o s3
+aws s3 cp myfolder s3://mybucket/myfolder --recursive --storage-class GLACIER
